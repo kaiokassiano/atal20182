1509166f9e2bd99e31e446ff2254b69a8df61405
kaiokassiano <kaio.kassiano.oliveira@ccc.ufcg.edu.br>
2018-10-09 11:39:13 -0300

    Modify solution to sort in feasible time
    A modified merge sort is beeing implemented, in order to optimize
    sorting time.
    
    This mergesort is a hybrid between the 'classic' merge sort algorithm
    and the insertion sort algorithm. If the list size is lesser than 5,
    the solution will apply a insertion sort on the list, instead of a
    merge sort (when the size would get to 1).
    
    Both merge sort and insertion sort implementations here are sorting
    in descending order. The related changes are highlighted as comments
    in the solution.

--- atal/algorithms.py
+++ atal/algorithms.py
@@ -1,8 +1,54 @@
 # Esse metodo recebe uma lista com as matriculas dos alunos
 # e retorna essa lista em ordem crescente de matriculas
 def retorna_matriculas_decrescente(alist):
-	for j in xrange(len(alist)):
-		for i in xrange(len(alist)-1):
-			if alist[i] > alist[i+1]:
-				alist[i], alist[i+1] = alist[i+1], alist[i]
-	return alist
+    return mergesort(alist)
+
+ 
+def mergesort(alist):
+    if len(alist) <= 4:
+        return insertionsort(alist)
+ 
+    middle = len(alist) // 2
+    left = alist[:middle]
+    right = alist[middle:]
+ 
+    left = mergesort(left)
+    right = mergesort(right)
+    return list(merge(left, right))
+
+
+def merge(left, right):
+    result = []
+    left_index = 0
+    right_index = 0
+
+    while left_index < len(left) and right_index < len(right):
+        # comparison is ">=" to sort in descending order
+        if left[left_index] >= right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
+ 
+    if left_index < len(left):
+        result.extend(left[left_index:])
+    if right_index < len(right):
+        result.extend(right[right_index:])
+
+    return result
+
+
+def insertionsort(alist):
+    for i in xrange(1, len(alist)):
+        j = i-1 
+        element = alist[i]
+
+        # comparison is "<" to sort in descending order
+        while (alist[j] < element) and (j >= 0):
+           alist[j +1] = alist[j]
+           j -= 1
+
+        alist[j +1] = element
+    
+    return alist
